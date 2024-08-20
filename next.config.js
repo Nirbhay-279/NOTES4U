bf49186a4aab328e7b369d15e2f29fd792825677
Nirbhay <143536386+Nirbhay-279@users.noreply.github.com>
2024-08-21 01:49:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["firebasestorage.googleapis.com"],
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
-  
-
-// next.config.js
-module.exports = {
   images: {
     domains: ['firebasestorage.googleapis.com', 'res.cloudinary.com'],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
+
+module.exports = nextConfig;
